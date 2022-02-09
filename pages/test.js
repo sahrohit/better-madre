4d474c02c86267172ba08594c1869f431cac1a70
sahrohit <sahrohit9586@hmail.com>
2022-02-09 17:29:40 +0545

    Added Test File

--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,53 @@
 import React from "react";
-import FullPageLoadingSpinner from "@components/shared/FullPageLoadingSpinner";
+import { Button, useToast } from "@chakra-ui/react";
+import {
+	collection,
+	addDoc,
+	doc,
+	setDoc,
+	updateDoc,
+	onSnapshot,
+	getDocs,
+	deleteField,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { useMenu } from "../contexts/MenuContext";
 
-const test = () => {
-	return <FullPageLoadingSpinner />;
+const TestingPage = () => {
+	const { menuItems } = useMenu();
+
+	const toast = useToast();
+
+	const applyChanges = () => {
+		// try {
+		// 	menuItems.map((item) => {
+		// 		console.log(item.menuId);
+		// 		updateDoc(doc(db, "menu", item.menuId), {
+		// 			badges: ["New", "Popular"],
+		// 			isNew: deleteField(),
+		// 			isPopular: deleteField(),
+		// 		});
+		// 	});
+		// 	toast({
+		// 		title: "Success",
+		// 		description: "Changes applied",
+		// 		status: "success",
+		// 		duration: 9000,
+		// 		isClosable: true,
+		// 	});
+		// } catch (error) {
+		// 	console.log(error);
+		// 	toast({
+		// 		title: "An Error Occured",
+		// 		description: error.message,
+		// 		status: "error",
+		// 		duration: 9000,
+		// 		isClosable: true,
+		// 	});
+		// }
+	};
+
+	return <Button onClick={applyChanges}>Apply Changes</Button>;
 };
 
-export default test;
+export default TestingPage;
