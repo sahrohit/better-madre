b3ec0fc232bd1fcc617250c10674abc0c09f30c5
sahrohit <sahrohit9586@hmail.com>
2022-02-12 22:44:16 +0545

    Configured next-pwa for PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
+	pwa: {
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		runtimeCaching,
+	},
 	swcMinify: true,
 	reactStrictMode: true,
 	images: {
 		domains: ["source.unsplash.com", "lh3.googleusercontent.com"],
 	},
-};
+});
