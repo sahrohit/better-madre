9ec3d2a7a58bc811594b18428c8bbdbe4f6d51a3
sahrohit <sahrohit9586@hmail.com>
2022-02-12 22:44:58 +0545

    Boiler Plate Pages for Navbar Options

---
+++ pages/aboutus.js
@@ -0,0 +1,5 @@
+const AboutUsPage = () => {
+	return <h1>This is AboutUsPage</h1>;
+};
+
+export default AboutUsPage;
---
+++ pages/jobs.js
@@ -0,0 +1,5 @@
+const JobsPage = () => {
+	return <h1>This is JobsPage</h1>;
+};
+
+export default JobsPage;
