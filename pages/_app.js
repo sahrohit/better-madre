ee819c46aeda069528d872e7175d37bb87d6c252
sahrohit <sahrohit9586@hmail.com>
2022-02-16 20:20:47 +0545

    Added Not a Product Alert (commented)

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { AuthProvider } from "@contexts/AuthContext";
 import { MenuProvider } from "@contexts/MenuContext";
 import { UserProvider } from "@contexts/UserContext";
 
+import TopAlert from "@components/shared/TopAlert";
+
 import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
@@ -19,6 +21,7 @@ function MyApp({ Component, pageProps }) {
 			<AuthProvider>
 				<MenuProvider>
 					<UserProvider>
+						{/* <TopAlert /> */}
 						<Component {...pageProps} />
 					</UserProvider>
 				</MenuProvider>
